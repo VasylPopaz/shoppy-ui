6cbe85d4a62e7e99b56aeb2d0ccf06a51386bc5d
VasylPopaz <pafos1997@gmail.com>
2025-04-03 21:00:41 +0200

    Add signup form to the page

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,16 +1,40 @@
+"use client";
+
 import { Button, Link, Stack, TextField } from "@mui/material";
 import NextLink from "next/link";
+import { useFormState } from "react-dom";
+import { createUser } from "./create-user";
 
 const Signup = () => {
+  const [state, formAction] = useFormState(createUser, { error: "" });
+
   return (
-    <Stack spacing={2} className="w-full max-w-xs">
-      <TextField label="Email" type="email" variant="outlined" />
-      <TextField label="Password" type="password" variant="outlined" />
-      <Button variant="contained">Sign up</Button>
-      <Link component={NextLink} href="/auth/signin" className="self-center">
-        Signin
-      </Link>
-    </Stack>
+    <form action={formAction} className="w-full max-w-xs">
+      <Stack spacing={2}>
+        <TextField
+          name="email"
+          label="Email"
+          type="email"
+          variant="outlined"
+          helperText={state.error}
+          error={!!state.error}
+        />
+        <TextField
+          name="password"
+          label="Password"
+          type="password"
+          variant="outlined"
+          helperText={state.error}
+          error={!!state.error}
+        />
+        <Button type="submit" variant="contained">
+          Sign up
+        </Button>
+        <Link component={NextLink} href="/auth/signin" className="self-center">
+          Signin
+        </Link>
+      </Stack>
+    </form>
   );
 };
 
