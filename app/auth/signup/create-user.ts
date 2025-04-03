0d93713ac54e01b9f1522af6541306a68dbf4221
VasylPopaz <pafos1997@gmail.com>
2025-04-03 20:59:01 +0200

    Add server action for user creation

---
+++ app/auth/signup/create-user.ts
@@ -0,0 +1,19 @@
+"use server";
+
+import { redirect } from "next/navigation";
+import { API_URL } from "@/app/constants/api";
+import { getErrorMessage } from "@/app/util/errors";
+
+export const createUser = async (_prevState: any, formData: FormData) => {
+  const res = await fetch(`${API_URL}/users`, {
+    method: "POST",
+    body: formData,
+  });
+  const parsedRes = await res.json();
+
+  if (!res.ok) {
+    return { error: getErrorMessage(parsedRes) };
+  }
+
+  redirect("/");
+};
