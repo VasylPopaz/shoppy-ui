786ab671e6d555cbbdd1d3440a193682f117d641
VasylPopaz <pafos1997@gmail.com>
2025-04-12 21:03:35 +0200

    Add server action for signin, including JWT cookie storage and redirect

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,16 +1,41 @@
+"use client";
+
 import { Button, Link, Stack, TextField } from "@mui/material";
 import NextLink from "next/link";
+import { useActionState } from "react";
+
+import { signin } from "./signin";
 
 const Signin = () => {
+  const [state, formAction] = useActionState(signin, { error: "" });
+
   return (
-    <Stack spacing={2} className="w-full max-w-xs">
-      <TextField label="Email" type="email" variant="outlined" />
-      <TextField label="Password" type="password" variant="outlined" />
-      <Button variant="contained">Sign in</Button>
-      <Link component={NextLink} href="/auth/signup" className="self-center">
-        Signup
-      </Link>
-    </Stack>
+    <form action={formAction} className="w-full max-w-xs">
+      <Stack spacing={2}>
+        <TextField
+          name="email"
+          label="Email"
+          type="email"
+          variant="outlined"
+          helperText={state.error}
+          error={!!state.error}
+        />
+        <TextField
+          name="password"
+          label="Password"
+          type="password"
+          variant="outlined"
+          helperText={state.error}
+          error={!!state.error}
+        />
+        <Button type="submit" variant="contained">
+          Sign in
+        </Button>
+        <Link component={NextLink} href="/auth/signup" className="self-center">
+          Signup
+        </Link>
+      </Stack>
+    </form>
   );
 };
 
