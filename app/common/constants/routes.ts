aa22ed9050892aeaf06f6d11e18b77b6a9019914
VasylPopaz <pafos1997@gmail.com>
2025-04-13 23:05:20 +0200

    Add routes and unauthenticatedRoutes constants

---
+++ app/common/constants/routes.ts
@@ -0,0 +1,14 @@
+export const unauthenticatedRoutes = [
+  {
+    title: "Signin",
+    path: "/auth/signin",
+  },
+  { title: "Signup", path: "/auth/signup" },
+];
+
+export const routes = [
+  {
+    title: "Home",
+    path: "/",
+  },
+];
