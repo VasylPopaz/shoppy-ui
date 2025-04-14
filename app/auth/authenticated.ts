d9d80df389e01b758582eb9bbf7a86c820c03097
VasylPopaz <pafos1997@gmail.com>
2025-04-14 19:16:07 +0200

    Add AuthContext and authentication cookie check

---
+++ app/auth/authenticated.ts
@@ -0,0 +1,8 @@
+import { cookies } from "next/headers";
+
+import { AUTHENTICATION_COOKIE } from "./auth-cookie";
+
+export const authenticated = async () => {
+  const cookieStore = await cookies();
+  return !!cookieStore.get(AUTHENTICATION_COOKIE)?.value;
+};
