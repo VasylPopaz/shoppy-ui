d14cd5fde25ec3b5795987b44b9071f81bd4de1b
VasylPopaz <pafos1997@gmail.com>
2025-04-25 22:21:48 +0200

    Add product card

---
+++ app/products/product.tsx
@@ -0,0 +1,15 @@
+import { Card, Typography } from "@mui/material";
+import { Product as IProduct } from "./interfaces/product.interface";
+
+interface ProductProps {
+  product: IProduct;
+}
+export const Product = ({ product }: ProductProps) => {
+  return (
+    <Card className="p-4">
+      <Typography variant="h4">{product.name}</Typography>
+      <Typography>{product.description}</Typography>
+      <Typography>${product.price}</Typography>
+    </Card>
+  );
+};
